dd444de1ec1458445c0267dc1a7e402ff72284ac
Donato Borrello <venar303@gmail.com>
2014-01-04 20:37:10 -0500

    Removing race specific optimization

--- sabotage_offrace_weapons.user.js
+++ sabotage_offrace_weapons.user.js
@@ -2,7 +2,7 @@
 // @name            Sabotage Offrace Weapons
 // @namespace       http://www.kingsofchaos.com
 // @description     Adds all possible weapons
-// @version         1.1
+// @version         1.2
 // @include         http://*kingsofchaos.com/attack.php*
 // @require         http://cdnjs.cloudflare.com/ajax/libs/jquery/1.10.2/jquery.min.js
 // @require         http://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.5.2/underscore-min.js
@@ -14,12 +14,11 @@ var raceSpecific = {
     Orcs    : '<option label="Club" value="14">Club</option><option label="Heavy Shield" value="49">Heavy Shield</option><option label="Warg" value="26">Warg</option><option label="Dragon" value="30">Dragon</option><option label="Warblade" value="22">Warblade</option><option label="Sling" value="10">Sling</option><option label="Spear" value="18">Spear</option>',
     Humans  : '<option label="Staff" value="7">Staff</option><option label="Broadsword" value="19">Broadsword</option><option label="Long Sword" value="11">Long Sword</option><option label="Excalibur" value="27">Excalibur</option><option label="Steed"value="23">Steed</option><option label="Lance" value="15">Lance</option><option label="Mithril" value="46">Mithril</option>',
     Undead  : '<option label="Scimitar" value="5">Scimitar</option><option label="Mist Veil" value="50">Mist Veil</option><option label="Dragon Claw" value="6">Dragon Claw</option>'
-}
+};
 
-var raceStylesheet = $('head > link:eq(3)').attr('href');
-var race = raceStylesheet.split('/').pop().split('.')[0];
-
-// We don't need to add the ones which are already there
-delete raceSpecific[race];
+// // We don't need to add the ones which are already there
+// var raceStylesheet = $('head > link:eq(3)').attr('href');
+// var race = raceStylesheet.split('/').pop().split('.')[0];
+// delete raceSpecific[race];
 
 $('select[name="enemy_weapon"]').append(_.values(raceSpecific));
